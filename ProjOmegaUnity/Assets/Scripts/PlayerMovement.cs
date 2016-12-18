a3def97c91ad1cf23ef0478e3f03905495025ba0
FALLENxGaLaXie5 <steward.joshua@rocketmail.com>
2016-12-18 15:07:20 -0800

    init commit with player

---
+++ ProjOmegaUnity/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,56 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+
+    private float speedCopy;
+
+    public float increasedSpeed;
+
+    private Vector3 targetPosition;
+
+    Rigidbody playerObject;
+
+    //Animator anim;
+
+    private bool isMoving;
+    void Start()
+    {
+        playerObject = GetComponent<Rigidbody>();
+        speedCopy = speed;
+        isMoving = false;
+        //anim = GetComponent<Animator>();
+    }
+
+
+    void Update()
+    {
+        if (Input.GetKey("w") || Input.GetKey("s") || Input.GetKey("a") || Input.GetKey("d"))
+        {
+            isMoving = true;
+            //anim.SetBool("walking", true);
+        }
+        else
+        {
+            isMoving = false;
+            //anim.SetBool("walking", false);
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if(isMoving)
+        {
+            MovePlayer();
+        }
+    }
+
+    void MovePlayer()
+    {
+        playerObject.AddForce(transform.forward * speed);
+    }
+
+
+}
